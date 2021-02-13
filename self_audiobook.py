20c99cd765e0e5f93ea3d35b005e185e3d2a9633
addy100 <72308696+addy100@users.noreply.github.com>
2021-02-13 23:40:22 +0530

    uploaded

---
+++ self_audiobook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+##Self Audiobook
+import pyttsx3
+
+
+# In[3]:
+
+
+speaker = pyttsx3.init()
+speaker.say('Hey! I like you')
+speaker.runAndWait()
+
+
+# In[ ]:
+
+
+
+
