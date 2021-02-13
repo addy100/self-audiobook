d992f3a28da46d0bda91d1c4c416b3425a997ebb
addy100 <72308696+addy100@users.noreply.github.com>
2021-02-13 23:57:44 +0530

    upp

--- self_audiobook.py
+++ self_audiobook.py
@@ -2,22 +2,30 @@
 # coding: utf-8
 
 # In[1]:
-
-
 ##Self Audiobook
 import pyttsx3
 
 
-# In[3]:
-
-
+# In[2]:
 speaker = pyttsx3.init()
 speaker.say('Hey! I like you')
 speaker.runAndWait()
 
 
-# In[ ]:
+# In[3]:
+## read content from a foreign file (pdf, docx)
+import PyPDF2
 
 
+# In[4]:
+book = open('C:\Users\RISING-PEGASUS 007\Desktop\skull candy Warranty Claim Form,pdf', rb)
+reader = PyPDF2.PdfFileReaader(book)
 
+pages = pdfReader.numPages
+print(pages)
 
+##To get any random page num
+pg = pdfReader.getPage()
+
+## To
+text = pg.extractext()
